e57b23efce3e5890c806d22cda3a25279441c7d5
DavePearce <david.pearce@ecs.vuw.ac.nz>
2022-05-25 12:12:04 +1200

    In principle it now does something.
    I need to add some tests next.  Then it will fail, and I need to update the
    parser.

--- src/whiley_file.rs
+++ src/whiley_file.rs
@@ -1,3 +1,16 @@
+use crate::ast::AbstractSyntaxTree;
+
 struct WhileyFile {
-    
+    ast : Box<AbstractSyntaxTree>
+}
+
+impl WhileyFile {
+    pub fn from_str(input: &str) {
+	let mut ast = AbstractSyntaxTree::new();
+	let mut parser = Parser::new(input,&mut ast, source_mapper);
+	// Parse the file
+	
+	// Done
+	WhileyFile{ast:Box::new(ast)}
+    }
 }
